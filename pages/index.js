ffe29b1bef09b4a29f86f59dde3956203e2309d7
karimabdurrahman <52180225+karimabdurrahman@users.noreply.github.com>
2022-06-02 15:34:28 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Valar Moghulis" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Bang, kalau mau ke bawah kasih tau ye bang
         </p>
       </main>
 
