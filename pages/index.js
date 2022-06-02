c36cde90a96b9709a67065a4f4a8fafed02e23c7
karimabdurrahman <52180225+karimabdurrahman@users.noreply.github.com>
2022-06-02 15:31:47 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>durra hiding spot 3.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Valar Moghulis" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
